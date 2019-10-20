9c119cd360a474dea19bf0cd2a50baeb655469b9
ZUBAIR AHMED <zubair1291@gmail.com>
2019-10-20 17:18:43 +0200

    Initial Commit

---
+++ Swift-MVVM-Shell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-MVVM-Shell
+//
+//  Created by Zubair Ahmed on 20.10.19.
+//  Copyright © 2019 Zubair Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift-MVVM-ShellTests/Swift_MVVM_ShellTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift_MVVM_ShellTests.swift
+//  Swift-MVVM-ShellTests
+//
+//  Created by Zubair Ahmed on 20.10.19.
+//  Copyright © 2019 Zubair Ahmed. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_MVVM_Shell
+
+class Swift_MVVM_ShellTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
